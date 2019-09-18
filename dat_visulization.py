d2d7a42a6ecd910fb7d9acb50cd5150945b0cd2b
biplab malakar <biplab@terriblytinytales.com>
2019-09-18 23:20:42 +0530

    histogram, kde, scatter color

--- dat_visulization.py
+++ dat_visulization.py
@@ -63,5 +63,54 @@ sns.regplot(x=scatterData['shares'], y=scatterData['likes'])
 plt.xlabel('Shares')
 plt.ylabel('Likes')
 
+#color code scatter graph which will explin day of week analyze
 
+weekFilePath='./week_day.csv'
+weekShareLikeData = pd.read_csv(weekFilePath)
+plt.title('Scatter graph with color week day explain')
+sns.scatterplot(x=weekShareLikeData['shares'], y=weekShareLikeData['likes'], hue=weekShareLikeData['week_day'])
+plt.xlabel('Shares')
+plt.ylabel('Likes')
 
+#add more regression line corresponding to the week day 1,2,3,4,5,6,7
+
+sns.lmplot(x='shares', y="likes", hue="week_day", data=weekShareLikeData)
+
+#catgorize scatter graph based on week_day likes and share relationship
+
+sns.swarmplot(x=weekShareLikeData['week_day'],y=weekShareLikeData['likes'])
+
+#histogram graph, display histograp chart of shares(group by numbers)
+sns.distplot(a=weekShareLikeData['shares'], ked=False) # a, select column want to plot and kde is False for histogram data
+#if kde ie true, then mix of density and histogram graph
+#KDE kernal density estimate, smotth histogram
+sns.kdeplot(data=weekShareLikeData['shares'], shade=True) # shade true, color the space below line
+
+#we plot only one column in kde, we have 2 column also to see their effet and relationship
+#we call this 2D-KDE(jointplot)
+sns.jointplot(x=weekShareLikeData['shares'], y=weekShareLikeData['likes'], kind='kde') #kind, kde means density graph
+
+#color coded histogram for mix data from different file
+iris_set_filepath = "../input/iris_setosa.csv"
+iris_ver_filepath = "../input/iris_versicolor.csv"
+iris_vir_filepath = "../input/iris_virginica.csv"
+
+# Read the files into variables 
+iris_set_data = pd.read_csv(iris_set_filepath, index_col="Id")
+iris_ver_data = pd.read_csv(iris_ver_filepath, index_col="Id")
+iris_vir_data = pd.read_csv(iris_vir_filepath, index_col="Id")
+
+#graph for each file data
+sns.distplot(a=iris_set_data['Petal Length (cm)'], label="Iris-setosa", kde=False)
+sns.distplot(a=iris_ver_data['Petal Length (cm)'], label="Iris-versicolor", kde=False)
+sns.distplot(a=iris_vir_data['Petal Length (cm)'], label="Iris-virginica", kde=False)
+
+#day going to combine together in single graph
+
+# Add title
+plt.title("Histogram of Petal Lengths, by Species")
+
+# Force legend to appear
+plt.legend()
+
+#we can do same for kde also
